07a31680dad077566bc79b15d2611063b91e8e5c
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-09 17:49:56 -0500

    working on animating

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -2,5 +2,7 @@ let topBlock = document.querySelectorAll('.block');
 
 let topArray = Array.from(topBlock); 
 
-topArray.forEach(block => block.addEventListener('click', function() {block.parentNode.prepend(block)}));
+topArray.forEach(function(block) {
+    block.addEventListener('click', block => block.parentNode.prepend(block));
     // block.addEventListener('mousedown', () => block.style.transform = translate(1000)); 
+}); 
