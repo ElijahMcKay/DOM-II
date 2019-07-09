223d426bc269d48b53c7698c4b8249c812cde32e
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-09 17:35:43 -0500

    finished functionality to reorder blocks when you click them

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,9 +1,6 @@
-let topBlock = document.querySelectorAll('.block'); 
+let topBlock = document.querySelectorAll('.block');
 
-let topBlockArray = Array.from(topBlock); 
+let topArray = Array.from(topBlock); 
 
-topBlockArray.forEach(function(block) {
-    block.addEventListener('click', () => topBlock.prepend());
+topArray.forEach(block => block.addEventListener('click', function() {block.parentNode.prepend(block)}));
     // block.addEventListener('mousedown', () => block.style.transform = translate(1000)); 
-}); 
-
